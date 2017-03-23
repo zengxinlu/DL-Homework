21cccb12fa4708acee9c9748b73eb161fca26531
zxl <zxl@zxldeMacBook-Pro.local>
2017-03-23 15:23:22 +0800

    初始化minst

--- feed.py
+++ feed.py
@@ -11,15 +11,12 @@ import mnist
 FLAGS = None
 
 def placeholder_inputs(batch_size):
-  images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
-                                                         mnist.IMAGE_PIXELS))
+  images_placeholder = tf.placeholder(tf.float32, shape=(batch_size, mnist.IMAGE_PIXELS))
   labels_placeholder = tf.placeholder(tf.int32, shape=(batch_size))
   return images_placeholder, labels_placeholder
 
-
 def fill_feed_dict(data_set, images_pl, labels_pl):
-  images_feed, labels_feed = data_set.next_batch(FLAGS.batch_size,
-                                                 FLAGS.fake_data)
+  images_feed, labels_feed = data_set.next_batch(FLAGS.batch_size, FLAGS.fake_data)
   feed_dict = {
       images_pl: images_feed,
       labels_pl: labels_feed,
@@ -44,56 +41,38 @@ def do_eval(sess, eval_correct, images_placeholder, labels_placeholder, data_set
 def run_training():
     data_sets = input_data.read_data_sets(FLAGS.input_data_dir, FLAGS.fake_data)
     with tf.Graph().as_default():
-        images_placeholder, labels_placeholder = placeholder_inputs(
-            FLAGS.batch_size)
-        logits = mnist.inference(images_placeholder,
-                                FLAGS.hidden1,
-                                FLAGS.hidden2)
+        # 初始化输入占位符
+        images_placeholder, labels_placeholder = placeholder_inputs(FLAGS.batch_size)
+        # 构建神经网络
+        logits = mnist.inference(images_placeholder,FLAGS.hidden1,FLAGS.hidden2)
+        # 添加损失函数
         loss = mnist.loss(logits, labels_placeholder)
+        # 训练
         train_op = mnist.training(loss, FLAGS.learning_rate)
+        # 正确率计算
         eval_correct = mnist.evaluation(logits, labels_placeholder)
 
         init = tf.global_variables_initializer()
-        saver = tf.train.Saver()
         sess = tf.Session()
         sess.run(init)
 
         for step in range(FLAGS.max_steps):
             start_time = time.time()
-            feed_dict = fill_feed_dict(data_sets.train,
-                                        images_placeholder,
-                                        labels_placeholder)
-            _, loss_value = sess.run([train_op, loss],
-                                    feed_dict=feed_dict)
+
+            feed_dict = fill_feed_dict(data_sets.train, images_placeholder, labels_placeholder)
+            _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
 
             duration = time.time() - start_time
-
             if step % 100 == 0:
                 print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
 
-            # Save a checkpoint and evaluate the model periodically.
             if (step + 1) % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-                # Evaluate against the training set.
                 print('Training Data Eval:')
-                do_eval(sess,
-                        eval_correct,
-                        images_placeholder,
-                        labels_placeholder,
-                        data_sets.train)
-                # Evaluate against the validation set.
+                do_eval(sess, eval_correct, images_placeholder, labels_placeholder, data_sets.train)
                 print('Validation Data Eval:')
-                do_eval(sess,
-                        eval_correct,
-                        images_placeholder,
-                        labels_placeholder,
-                        data_sets.validation)
-                # Evaluate against the test set.
+                do_eval(sess, eval_correct, images_placeholder, labels_placeholder, data_sets.validation)
                 print('Test Data Eval:')
-                do_eval(sess,
-                        eval_correct,
-                        images_placeholder,
-                        labels_placeholder,
-                        data_sets.test)
+                do_eval(sess, eval_correct, images_placeholder, labels_placeholder, data_sets.test)
 
 
 if __name__ == '__main__':
